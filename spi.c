cf33fb8c39a093ff63b9a1c463d385c4cfb5700a
Daniel Loffgren <daniel@loffgren.org>
2015-08-03 20:45:31 -0700

    Reindented the code and gave it a message to make it obvious when it is called

--- spi.c
+++ spi.c
@@ -1,8 +1,7 @@
 #include <CoreGraphics/CoreGraphics.h>
 	
-CGError
-CGSSetWindowDepthLimit(uint32_t cid, uint32_t wid, uint32_t limit)
-{
-	  return kCGErrorSuccess;
+CGError CGSSetWindowDepthLimit(uint32_t cid, uint32_t wid, uint32_t limit) {
+	fprintf(stderr, "CGSSetWindowDepthLimit was called, the libNova version was used.\n");
+	return kCGErrorSuccess;
 }
 
