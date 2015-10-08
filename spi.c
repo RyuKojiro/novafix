b948545750af1766af1666d3ae808db31a56b842
Daniel Loffgren <daniel@loffgren.org>
2015-10-07 19:59:42 -0700

    Arg names don't matter

--- spi.c
+++ spi.c
@@ -1,6 +1,6 @@
 #include <CoreGraphics/CoreGraphics.h>
 	
-CGError CGSSetWindowDepthLimit(uint32_t cid, uint32_t wid, uint32_t limit) {
+CGError CGSSetWindowDepthLimit(uint32_t arg1, uint32_t arg2, uint32_t arg3) {
 	fprintf(stderr, "CGSSetWindowDepthLimit was called, the libNova version was used.\n");
 	return kCGErrorSuccess;
 }
