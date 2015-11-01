c8266b644858bf235599308a05e3955defd6709a
res1233 <res1233+github@gmail.com>
2015-11-01 14:03:53 -0500

    Update launcher.sh
    more versatile syntax. Can be run from any directory.

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export DYLD_INSERT_LIBRARIES=${PWD}/libNova.A.dylib
+export DYLD_INSERT_LIBRARIES=`dirname $0`/libNova.A.dylib
 export DYLD_FORCE_FLAT_NAMESPACE=1
 
 /Applications/EV\ Nova.app/Contents/MacOS/EV\ Nova
