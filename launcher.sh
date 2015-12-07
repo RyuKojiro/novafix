e042cc2026b852bde2b5b16f8f2ab1645a219a7b
Richard Yeh <apocryphon.now@gmail.com>
2015-12-06 23:10:52 -0800

    Updated DYLD_INSERT_LIBRARIES to point at GLEngine in the OpenGL framework to resolve: http://stackoverflow.com/questions/22732381/dyld-symbol-not-found-but-nm-reports-otherwise-os-x-update-issue

--- launcher.sh
+++ launcher.sh
@@ -3,7 +3,7 @@ WD=`dirname "$0"`
 EVBIN="Ev Nova"
 
 export DYLD_FORCE_FLAT_NAMESPACE=1
-export DYLD_INSERT_LIBRARIES="$WD/libNova.A.dylib"
+export DYLD_INSERT_LIBRARIES="$WD/libNova.A.dylib:/System/Library/Frameworks/OpenGL.framework/Resources/GLEngine.bundle/GLEngine"
 
 if [ -e "$WD/$EVBIN.original" ]
 then
