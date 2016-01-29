173e39a553f261c5ec04ef047067cfcbea79b0ab
Richard Yeh <apocryphon.now@gmail.com>
2016-01-28 21:39:39 -0800

    added check for GLEngine, before setting DYLD_INSERT_LIBRARIES to it

--- launcher.sh
+++ launcher.sh
@@ -3,7 +3,13 @@ WD=`dirname "$0"`
 EVBIN="Ev Nova"
 
 export DYLD_FORCE_FLAT_NAMESPACE=1
-export DYLD_INSERT_LIBRARIES="$WD/libNova.A.dylib:/System/Library/Frameworks/OpenGL.framework/Resources/GLEngine.bundle/GLEngine"
+
+if [ -e "/System/Library/Frameworks/OpenGL.framework/Resources/GLEngine.bundle/GLEngine" ]
+then
+	export DYLD_INSERT_LIBRARIES="$WD/libNova.A.dylib:/System/Library/Frameworks/OpenGL.framework/Resources/GLEngine.bundle/GLEngine"
+else
+	export DYLD_INSERT_LIBRARIES="$WD/libNova.A.dylib"
+fi
 
 if [ -e "$WD/$EVBIN.original" ]
 then
