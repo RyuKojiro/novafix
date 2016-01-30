0d0190982ba92ca8781232208ecf0ae1e3bfac86
Daniel Loffgren <daniel@loffgren.org>
2016-01-30 11:28:35 -0800

    Cleaned up the GLEngine appending code a bit

--- launcher.sh
+++ launcher.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 WD=`dirname "$0"`
 EVBIN="Ev Nova"
+GLENGINE="/System/Library/Frameworks/OpenGL.framework/Resources/GLEngine.bundle/GLEngine"
 
 export DYLD_FORCE_FLAT_NAMESPACE=1
+export DYLD_INSERT_LIBRARIES="$WD/libNova.A.dylib"
 
-if [ -e "/System/Library/Frameworks/OpenGL.framework/Resources/GLEngine.bundle/GLEngine" ]
+if [ -e $GLENGINE ]
 then
-	export DYLD_INSERT_LIBRARIES="$WD/libNova.A.dylib:/System/Library/Frameworks/OpenGL.framework/Resources/GLEngine.bundle/GLEngine"
-else
-	export DYLD_INSERT_LIBRARIES="$WD/libNova.A.dylib"
+	export DYLD_INSERT_LIBRARIES="$DYLD_INSERT_LIBRARIES:$GLENGINE"
 fi
 
 if [ -e "$WD/$EVBIN.original" ]
