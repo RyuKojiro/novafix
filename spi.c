c19a63e8c43873fce5f2ee1211dda70c3d315056
Daniel Loffgren <daniel@loffgren.org>
2018-11-14 01:07:30 -0800

    Removed the printf, so that we don't have to worry about linking against anything, eliminating SDK probems with the removal of 32-bit

--- spi.c
+++ spi.c
@@ -1,7 +1,6 @@
 #include <CoreGraphics/CoreGraphics.h>
 	
 CGError CGSSetWindowDepthLimit(uint32_t arg1, uint32_t arg2, uint32_t arg3) {
-	fprintf(stderr, "CGSSetWindowDepthLimit was called, the libNova version was used.\n");
 	return kCGErrorSuccess;
 }
 
